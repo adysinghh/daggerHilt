892fd7134daa9c4f5ba443e4c1567d725fd20066
Aditya Singh <123162244+adysinghh@users.noreply.github.com>
2024-02-13 19:23:18 +0530

    added MyApp.kt

--- app/src/main/java/com/adyy/daggerhilt/MainActivity.kt
+++ app/src/main/java/com/adyy/daggerhilt/MainActivity.kt
@@ -10,37 +10,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.adyy.daggerhilt.ui.theme.DaggerHiltTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DaggerHiltTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+               val viewModel = hiltViewModel<MyViewModel>()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DaggerHiltTheme {
-        Greeting("Android")
     }
 }
